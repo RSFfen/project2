5c64d8b98e29505a3935e17a04e2a8abd6177a6e
RSFfen <rsf61.fen@gmail.com>
2021-06-12 12:25:23 +0300

    initial commit

--- pages/urls.py
+++ pages/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from .views import HomePageView, AboutPageView
 from .views import ZakyskiPageView
 from .views import SovetiPageView
+from .views import CommentCreateView
+
 urlpatterns = [
-    path('soveti/', SovetiPageView.as_view(), name='soveti'), # new
-    path('zakyski/', ZakyskiPageView.as_view(), name='zakyski'), # new
-    path('about/', AboutPageView.as_view(), name='about'), # new
+    path('soveti/', SovetiPageView.as_view(), name='soveti'),
+    path('zakyski/', ZakyskiPageView.as_view(), name='zakyski'),
+    path('about/', AboutPageView.as_view(), name='about'),
     path('', HomePageView.as_view(), name='home'),
+    path('comment_new/', CommentCreateView.as_view(), name='comment_new'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,14 @@
-from django.views.generic import TemplateView
-
+from django.views.generic import TemplateView, CreateView
+from .models import Comment
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+    context_object_name = 'all_comments_list'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_comments_list'] = Comment.objects.all()
+        return context
 
 class AboutPageView(TemplateView): # new
     template_name = 'about.html'
@@ -13,3 +18,8 @@ class ZakyskiPageView(TemplateView): # new
     
 class SovetiPageView(TemplateView): # new
     template_name = 'soveti.html'
+
+class CommentCreateView(CreateView):
+    model = Comment
+    template_name = 'comment_new.html'
+    fields = ('text',)
